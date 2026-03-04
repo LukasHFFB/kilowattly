7c3dddb6b668da46ffae2a708f0c76ec852d93df
Lukas Hoffbauer <lh@reel.energy>
2026-03-04 20:12:09 +0100

    fix: refactor Prisma Client for v7 adapter requirements

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const connectionString = process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL
 
 const prismaClientSingleton = () => {
+    if (connectionString) {
+        const pool = new Pool({ connectionString })
+        const adapter = new PrismaPg(pool)
+        return new PrismaClient({ adapter })
+    }
     return new PrismaClient()
 }
 
